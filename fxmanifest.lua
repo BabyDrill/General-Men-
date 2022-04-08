f3039556a1017c11d14969925fbfe3d75d6dccdf
BabyDrill <96079788+BabyDrill@users.noreply.github.com>
2022-04-08 17:40:12 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'BabyDrill'
+
+description 'Menu f5'
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/it.lua',
+	'client.lua',
+	'config.lua'
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/it.lua',
+	'config.lua',
+	'server.lua'
+}
+
+
